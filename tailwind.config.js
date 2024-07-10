5c6fb1e5a1bcadedfc05557e2f29ddba8f92fdc6
LeemoTseng-Mac <leemotseng@LeemoTseng-MacdeiMac.local>
2024-07-10 18:03:49 +0800

    navbar settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          'primary-color':'#F5AB01',
+          'secondary-color':'#4D4A48',
+      }
+    },
   },
   plugins: [],
 }
