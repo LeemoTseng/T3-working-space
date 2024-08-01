64ad43b1899dfb26e5c8e5b1340d6ae25abdc3ab
LeemoTseng-Mac <leemotseng@LeemoTseng-MacdeiMac.local>
2024-08-01 16:32:54 +0800

    adding memberships - plans

--- src/main.js
+++ src/main.js
@@ -2,24 +2,13 @@
 // *---------------* //
 //
 // Index
-// $0. Import
 // $1. Home page
 // $1.1 Features
+// $2. Plans
 //
 // *---------------* //
 
-// *---------------* //
-// $0. Import
-// *---------------* //
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 
 // *---------------* //
@@ -177,3 +166,162 @@ feature3.forEach(item => {
     document.querySelector('#feature3').appendChild(ul3);
 
 })
+
+
+// *---------------* //
+// $2. Plans
+// *---------------* //
+
+
+//plans
+const plans = [
+    {
+        title: 'HOT DESK',
+        currency: 'NT$',
+        price: '6,500',
+        priod: '/month',
+        from:'',
+        features: [
+            { text: 'Shared seating', available: true },
+            { text: '24/7 access', available: true },
+            { text: 'Complimentary coffee, beverages & snacks', available: true },
+            { text: 'Full event access', available: true },
+            { text: 'Fully-equipped meeting rooms and high speed internet', availible: false },
+            { text: 'Electric height adjustable desks', available: false },
+            { text: 'Ergonomic chairs', available: false },
+            { text: 'Secure and private rooms', available: false },
+        ]
+    },
+    {
+        title: 'DEDICATED DESK',
+        currency: 'NT$',
+        price: '10,000',
+        priod: '/month',
+        from:'from',
+        features: [
+            { text: 'Designated seating', available: true },
+            { text: '24/7 access', available: true },
+            { text: 'Electric height adjustable desks', available: true },
+            { text: 'Ergonomic chairs', available: true },
+            { text: 'Complimentary coffee, beverages & snacks', available: true },
+            { text: 'Fully-equipped meeting rooms and high speed internet', available: true },
+            { text: 'Full event access', available: true },
+            { text: 'Secure and private rooms', available: false },
+        ]
+    }, {
+        title: 'PRIVATE OFFICE',
+        currency: '',
+        price: 'Enquire now',
+        priod: '',
+        from:'',
+        features: [
+            { text: 'Designated seating', available: true },
+            { text: '24/7 access', available: true },
+            { text: 'Electric height adjustable desks', available: true },
+            { text: 'Ergonomic chairs', available: true },
+            { text: 'Complimentary coffee, beverages & snacks', available: true },
+            { text: 'Fully-equipped meeting rooms and high speed internet', available: true },
+            { text: 'Full event access', available: true },
+            { text: 'Secure and private rooms', available: true }
+        ]
+    }
+]
+
+
+plans.forEach(plan => {
+
+    // forEach plan card
+    const planDiv = document.createElement('div');
+    planDiv.className = 'plan flex flex-col max-w-lg';
+
+    const planContainer = document.createElement('div');
+    planContainer.className = 'container w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8'
+
+    const planTitle = document.createElement('h6');
+    planTitle.textContent = plan.title;
+
+    const itemsDiv = document.createElement('div');
+    itemsDiv.className = 'items flex  items-baseline text-gray-900'
+
+    const planCurrency = document.createElement('span');
+    planCurrency.className = 'text-3xl font-semibold';
+    planCurrency.textContent = plan.currency;
+
+    const planPrice = document.createElement('span');
+    planPrice.className = 'text-5xl font-extrabold tracking-tight'
+    planPrice.textContent = plan.price;
+
+    const planPeriod = document.createElement('span');
+    planPeriod.className = 'ms-1 text-xl font-normal text-gray-500'
+    planPeriod.textContent = plan.priod;
+
+    const fromSpan = document.createElement('span');
+    fromSpan.className = 'mr-2 text-gray-500';
+    fromSpan.textContent = plan.from;
+
+    itemsDiv.appendChild(fromSpan);
+    itemsDiv.appendChild(planCurrency);
+    itemsDiv.appendChild(planPrice);
+    itemsDiv.appendChild(planPeriod);
+
+    const featureList = document.createElement('ul');
+    featureList.className = 'space-y-5 my-7';
+    featureList.setAttribute('role', 'list');
+
+    // forEach feature
+    plan.features.forEach(feature => {
+        const featureItem = document.createElement('li');
+        featureItem.className = 'flex items-center';
+
+        const featureIcon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        featureIcon.setAttribute('class', 'flex-shrink-0 w-4 h-4');
+        featureIcon.setAttribute('aria-hidden', 'true');
+        featureIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        featureIcon.setAttribute('fill', 'currentColor');
+        featureIcon.setAttribute('viewBox', '0 0 24 24');
+
+        const featurePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        if (feature.available) {
+            featureIcon.classList.add('text-primary');
+            featurePath.setAttribute('d', 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z');
+        } else {
+            featureIcon.classList.add('text-gray-300');
+            featurePath.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z');
+        }
+
+        featureIcon.appendChild(featurePath);
+
+    const featureText = document.createElement('span');
+    featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'ms-3');
+
+    if (!feature.available) {
+      featureText.classList.add('text-gray-300');
+    }else{
+        featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'text-gray-600', 'ms-3')
+    }
+
+    featureText.textContent = feature.text;
+
+        featureItem.appendChild(featureIcon);
+        featureItem.appendChild(featureText);
+        featureList.appendChild(featureItem);
+    });
+
+    //Btn - Choose plan
+    const chooseButton = document.createElement('button');
+    chooseButton.type = 'button';
+    chooseButton.className = 'btn-default text-sm px-5 py-2.5 inline-flex justify-center w-full text-center';
+    chooseButton.textContent = 'Choose plan';
+
+    planContainer.appendChild(planTitle);
+    planContainer.appendChild(itemsDiv);
+    planContainer.appendChild(featureList);
+    planContainer.appendChild(chooseButton);
+
+    planDiv.appendChild(planContainer);
+
+    document.querySelector('#allPlan').appendChild(planDiv);
+
+})
+
