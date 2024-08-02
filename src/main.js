449ff65699ef06cb1ac662b5ec6ef463ad312065
LeemoTseng-Mac <leemotseng@LeemoTseng-MacdeiMac.local>
2024-08-02 17:39:52 +0800

    finished index.html git add .

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@
 // *---------------* //
 //
 // Index
-// $1. Home page
-// $1.1 Features
+//
+// $1. Features
 // $2. Plans
+// $3. Partners' logos
 //
 // *---------------* //
 
@@ -12,10 +13,8 @@
 
 
 // *---------------* //
-// $1. Home page
+// $1. Features
 // *---------------* //
-
-// $1.1 Features
 
 // Best Location and Facilities
 const ul = document.createElement('ul');
@@ -57,7 +56,7 @@ features1.forEach(text => {
     path.setAttribute('fill-rule', 'evenodd');
     path.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z');
     path.setAttribute('clip-rule', 'evenodd');
-    svg.appendChild(path);
+    svg.append(path);
 
     //span
     const span = document.createElement('span');
@@ -65,10 +64,9 @@ features1.forEach(text => {
     span.textContent = text;
 
     //append
-    li.appendChild(svg);
-    li.appendChild(span);
-    ul.appendChild(li);
-    document.querySelector('#feature1').appendChild(ul);
+    li.append(svg, span);
+    ul.append(li);
+    document.querySelector('#feature1').append(ul);
 
 })
 
@@ -104,7 +102,7 @@ features2.forEach(text => {
     path.setAttribute('fill-rule', 'evenodd');
     path.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z');
     path.setAttribute('clip-rule', 'evenodd');
-    svg.appendChild(path);
+    svg.append(path);
 
     //span
     const span = document.createElement('span');
@@ -112,10 +110,10 @@ features2.forEach(text => {
     span.textContent = text;
 
     //append
-    li.appendChild(svg);
-    li.appendChild(span);
-    ul2.appendChild(li);
-    document.querySelector('#feature2').appendChild(ul2);
+    li.append(svg, span);
+    li.append(span);
+    ul2.append(li);
+    document.querySelector('#feature2').append(ul2);
 
 })
 
@@ -148,11 +146,12 @@ feature3.forEach(item => {
     svg.setAttribute('fill', 'currentColor');
     svg.setAttribute('viewBox', '0 0 24 24');
 
+
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute('fill-rule', 'evenodd');
     path.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm13.707-1.293a1 1 0 0 0-1.414-1.414L11 12.586l-1.793-1.793a1 1 0 0 0-1.414 1.414l2.5 2.5a1 1 0 0 0 1.414 0l4-4Z');
     path.setAttribute('clip-rule', 'evenodd');
-    svg.appendChild(path);
+    svg.append(path);
 
     //span
     const span = document.createElement('span');
@@ -160,10 +159,9 @@ feature3.forEach(item => {
     span.textContent = item;
 
     //append
-    li.appendChild(svg);
-    li.appendChild(span);
-    ul3.appendChild(li);
-    document.querySelector('#feature3').appendChild(ul3);
+    li.append(svg, span);
+    ul3.append(li);
+    document.querySelector('#feature3').append(ul3);
 
 })
 
@@ -180,7 +178,7 @@ const plans = [
         currency: 'NT$',
         price: '6,500',
         priod: '/month',
-        from:'',
+        from: '',
         features: [
             { text: 'Shared seating', available: true },
             { text: '24/7 access', available: true },
@@ -197,7 +195,7 @@ const plans = [
         currency: 'NT$',
         price: '10,000',
         priod: '/month',
-        from:'from',
+        from: 'from',
         features: [
             { text: 'Designated seating', available: true },
             { text: '24/7 access', available: true },
@@ -213,7 +211,7 @@ const plans = [
         currency: '',
         price: 'Enquire now',
         priod: '',
-        from:'',
+        from: '',
         features: [
             { text: 'Designated seating', available: true },
             { text: '24/7 access', available: true },
@@ -259,16 +257,14 @@ plans.forEach(plan => {
     fromSpan.className = 'mr-2 text-gray-500';
     fromSpan.textContent = plan.from;
 
-    itemsDiv.appendChild(fromSpan);
-    itemsDiv.appendChild(planCurrency);
-    itemsDiv.appendChild(planPrice);
-    itemsDiv.appendChild(planPeriod);
+    itemsDiv.append(fromSpan, planCurrency, planPrice, planPeriod);
+
 
     const featureList = document.createElement('ul');
     featureList.className = 'space-y-5 my-7';
     featureList.setAttribute('role', 'list');
 
-    // forEach feature
+    // Feature focus
     plan.features.forEach(feature => {
         const featureItem = document.createElement('li');
         featureItem.className = 'flex items-center';
@@ -277,35 +273,41 @@ plans.forEach(plan => {
         featureIcon.setAttribute('class', 'flex-shrink-0 w-4 h-4');
         featureIcon.setAttribute('aria-hidden', 'true');
         featureIcon.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        featureIcon.setAttribute('fill', 'currentColor');
+        featureIcon.setAttribute('fill', 'none');
         featureIcon.setAttribute('viewBox', '0 0 24 24');
 
         const featurePath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        featurePath.setAttribute('stroke', 'currentColor');
+        featurePath.setAttribute('stroke-linecap', 'round');
+        featurePath.setAttribute('stroke-linejoin', 'round');
+        featurePath.setAttribute('stroke-width', '3');
+
 
         if (feature.available) {
             featureIcon.classList.add('text-primary');
-            featurePath.setAttribute('d', 'M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z');
+            featurePath.setAttribute('d', 'M5 11.917 9.724 16.5 19 7.5');
+
         } else {
             featureIcon.classList.add('text-gray-300');
-            featurePath.setAttribute('d', 'M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm7.707-3.707a1 1 0 0 0-1.414 1.414L10.586 12l-2.293 2.293a1 1 0 1 0 1.414 1.414L12 13.414l2.293 2.293a1 1 0 0 0 1.414-1.414L13.414 12l2.293-2.293a1 1 0 0 0-1.414-1.414L12 10.586 9.707 8.293Z');
+            featurePath.setAttribute('d', 'M6 18 17.94 6M18 18 6.06 6');
         }
 
-        featureIcon.appendChild(featurePath);
+        featureIcon.append(featurePath);
 
-    const featureText = document.createElement('span');
-    featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'ms-3');
+        const featureText = document.createElement('span');
+        featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'ms-3');
 
-    if (!feature.available) {
-      featureText.classList.add('text-gray-300');
-    }else{
-        featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'text-gray-600', 'ms-3')
-    }
+        if (!feature.available) {
+            featureText.classList.add('text-gray-300');
+        } else {
+            featureText.classList.add('text-base', 'font-normal', 'leading-tight', 'text-gray-600', 'ms-3')
+        }
 
-    featureText.textContent = feature.text;
+        featureText.textContent = feature.text;
 
-        featureItem.appendChild(featureIcon);
-        featureItem.appendChild(featureText);
-        featureList.appendChild(featureItem);
+
+        featureItem.append(featureIcon, featureText)
+        featureList.append(featureItem);
     });
 
     //Btn - Choose plan
@@ -314,14 +316,99 @@ plans.forEach(plan => {
     chooseButton.className = 'btn-default text-sm px-5 py-2.5 inline-flex justify-center w-full text-center';
     chooseButton.textContent = 'Choose plan';
 
-    planContainer.appendChild(planTitle);
-    planContainer.appendChild(itemsDiv);
-    planContainer.appendChild(featureList);
-    planContainer.appendChild(chooseButton);
+    planContainer.append(planTitle, itemsDiv, featureList, chooseButton);
+    planDiv.append(planContainer);
 
-    planDiv.appendChild(planContainer);
-
-    document.querySelector('#allPlan').appendChild(planDiv);
+    document.querySelector('#allPlan').append(planDiv);
 
 })
+
+// *---------------* //
+// $3. Partners' logos
+// *---------------* //
+//
+// ** Recommended type: png, horizaontal, 200x100(px) **
+//
+const partners = [
+    {
+        imgUrl: 'assets/mock/mock-vertical.png',
+        alt: 'mock',
+        title: 'Logo1',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-horizontal.png',
+        alt: 'mock',
+        title: 'Logo2',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-01.png',
+        alt: 'mock',
+        title: 'Logo3',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-02.png',
+        alt: 'mock',
+        title: 'Logo4',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-03.png',
+        alt: 'mock-vertical',
+        title: 'Logo5',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-04.png',
+        alt: 'mock',
+        title: 'Logo6',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-05.png',
+        alt: 'mock',
+        title: 'Logo7',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }, {
+        imgUrl: 'assets/mock/mock-06.png',
+        alt: 'mock',
+        title: 'Logo8',
+        description: 'lorem ipsum dolor sit amet, consectetur.',
+    }
+]
+
+partners.forEach( partner => {
+    const partnerDiv = document.createElement('div');
+    partnerDiv.classList.add('partner', 'flex', 'flex-col', 'items-center', 'p-2', 'hover:text-primary', 'cursor-pointer');
+
+    const imgBoxDiv = document.createElement('div');
+    imgBoxDiv.classList.add('imgBox', 'p-2', 'h-[120px]', 'max-w-[200px]')
+
+    const img = document.createElement('img');
+    img.classList.add('object-contain', 'w-full', 'h-full')
+    img.setAttribute('src', partner.imgUrl);
+    img.setAttribute('alt', partner.alt);
+
+    // console.log(img);
+
+    const titleP = document.createElement('p');
+    titleP.classList.add('font-bold', 'text-xl', 'mt-3');
+    titleP.textContent = partner.title;
+
+    const descriptionP = document.createElement('p');
+    descriptionP.classList.add('w-full', 'text-center', 'mt-2');
+
+    const descriptionSpan = document.createElement('span');
+    descriptionSpan.classList.add('text-slate-500');
+    descriptionSpan.textContent = partner.description;
+
+
+    imgBoxDiv.append(img);
+    descriptionP.append(descriptionSpan);
+    partnerDiv.append(imgBoxDiv, titleP, descriptionP);
+
+    document.querySelector('#partnersAll').append(partnerDiv);
+
+    // console.log(partnerDiv);
+
+})
+
+
+
 
