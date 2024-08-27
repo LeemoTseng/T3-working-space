7bb14bc33666842a640022c6e16c5081a2f87971
LeemoTseng-Mac <leemotseng@LeemoTseng-MacdeiMac.local>
2024-08-27 15:57:05 +0800

    update login page

--- src/management.js
+++ src/management.js
@@ -18,13 +18,51 @@
 
 
 if (document.querySelector('#login')) {
-    const hiddenPassword = document.querySelector('#hiddenPassword');
-    const showPassword = document.querySelector('#showPassword');
 
-    hiddenPassword.addEventListener('click',function(){
-        showPassword.classList.toggle('hidden');
-        hiddenPassword.classList.toggle('hidden');
+    // password input
+    const emailInput = document.querySelector('#email');
+    const passwordInput = document.querySelector('#password');
+    const hiddenIcon = document.querySelector('#hiddenIcon');
+    const showIcon = document.querySelector('#showIcon');
+
+    hiddenIcon.addEventListener('mousedown', function () {
+        hiddenIcon.classList.toggle('hidden');
+        showIcon.classList.toggle('hidden');
+        showIcon.classList.add('text-primary')
+        passwordInput.type = 'text';
     })
+
+    showIcon.addEventListener('mouseup', function () {
+        hiddenIcon.classList.toggle('hidden');
+        showIcon.classList.toggle('hidden');
+        passwordInput.type = 'password';
+
+    })
+
+    // error message
+    const errorMessage = document.querySelector('#errorMessage');
+    emailInput.addEventListener('input', function () {
+        if (emailInput.value === '' || !emailInput.value.includes('@')) {
+            errorMessage.classList.remove('hidden');
+        } else {
+            errorMessage.classList.add('hidden');
+            console.log(emailInput.value);
+        }
+    })
+
+
+    passwordInput.addEventListener('input', function () {
+        if (passwordInput.value === '') {
+            errorMessage.classList.remove('hidden');
+        } else {
+            errorMessage.classList.add('hidden');
+            console.log(passwordInput.value);
+        }
+    })
+
+
+
+
 
 } else {
     console.log('#login not found');
